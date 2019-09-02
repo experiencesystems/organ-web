32839e2b216e581a6660bcc127149c60ecfdf411
Natalya <natthy.aragao@gmail.com>
2019-09-02 20:34:26 -0300

    mapa

--- OrganWeb/OrganWeb/App_Start/BundleConfig.cs
+++ OrganWeb/OrganWeb/App_Start/BundleConfig.cs
@@ -28,9 +28,54 @@ namespace OrganWeb
             bundles.Add(new ScriptBundle("~/bundles/modal").Include(
                       "~/Scripts/modal.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/mapa").Include(
+                      "~/Scripts/src/leaflet-src.js",
+                      "~/Scripts/src/Leaflet.draw.js",
+                      "~/Scripts/src/Leaflet.Draw.Event.js",
+                      "~/Scripts/src/Toolbar.js",
+                      "~/Scripts/src/Tooltip.js",
+                      "~/Scripts/src/ext/GeometryUtil.js",
+                      "~/Scripts/src/ext/LatLngUtil.js",
+                      "~/Scripts/src/ext/LineUtil.Intersect.js",
+                      "~/Scripts/src/ext/Polygon.Intersect.js",
+                      "~/Scripts/src/ext/Polyline.Intersect.js",
+                      "~/Scripts/src/ext/TouchEvents.js",
+                      "~/Scripts/src/draw/DrawToolbar.js",
+                      "~/Scripts/src/draw/handler/Draw.Feature.js",
+                      "~/Scripts/src/draw/handler/Draw.SimpleShape.js",
+                      "~/Scripts/src/draw/handler/Draw.Polyline.js",
+                      "~/Scripts/src/draw/handler/Draw.Marker.js",
+                      "~/Scripts/src/draw/handler/Draw.CircleMarker.js",
+                      "~/Scripts/src/draw/handler/Draw.Circle.js",
+                      "~/Scripts/src/draw/handler/Draw.Polygon.js",
+                      "~/Scripts/src/draw/handler/Draw.Rectangle.js",
+                      "~/Scripts/src/src/edit/EditToolbar.js",
+                      "~/Scripts/src/edit/handler/EditToolbar.Edit.js",
+                      "~/Scripts/src/edit/handler/EditToolbar.Delete.js",
+                      "~/Scripts/src/Control.Draw.js",
+                      "~/Scripts/src/edit/handler/Edit.Poly.js",
+                      "~/Scripts/src/edit/handler/Edit.SimpleShape.js",
+                      "~/Scripts/src/edit/handler/Edit.Marker.js",
+                      "~/Scripts/src/edit/handler/Edit.CircleMarker.js",
+                      "~/Scripts/src/edit/handler/Edit.Circle.js",
+                      "~/Scripts/src/edit/handler/Edit.Rectangle.js"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/bundles/mapatela").Include(
+                      "~/Scripts/off-canvas.js"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/bundles/menu").Include(
+                      "~/Scripts/off-canvas.js",
+                      "~/Scripts/hoverable-collapse.js",
+                      "~/Scripts/template.js"
+                      ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
+                      "~/Content/leaflet.css",
+                      "~/Scripts/src/leaflet.draw.css",
                       "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/csslog").Include(
