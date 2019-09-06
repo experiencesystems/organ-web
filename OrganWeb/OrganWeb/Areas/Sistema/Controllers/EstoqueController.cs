08fd1e14219fc4fd370a3f6491b8fbd2400e180a
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-09-05 21:52:52 -0300

    Authorize redirecionando para a página de login, exemplo fazenda

---
+++ OrganWeb/OrganWeb/Areas/Sistema/Controllers/EstoqueController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OrganWeb.Areas.Sistema.Models;
+using System.Web.Mvc;
+using OrganWeb.Models;
+
+namespace OrganWeb.Areas.Sistema.Controllers
+{
+    [Authorize]
+    public class EstoqueController : Controller
+    {
+        private BancoContext db = new BancoContext();
+
+        // GET: Sistema/Estoque
+        public ActionResult Index()
+        {
+            var estoque = new ViewEstoque
+            {
+                VwItems = db.VwItems.ToList()
+            };
+
+            return View(estoque);
+        }
+    }
+}
--- OrganWeb/OrganWeb/Areas/Sistema/Controllers/FazendaController.cs
+++ OrganWeb/OrganWeb/Areas/Sistema/Controllers/FazendaController.cs
@@ -11,6 +11,7 @@ using OrganWeb.Models;
 
 namespace OrganWeb.Areas.Sistema.Controllers
 {
+    [Authorize]
     public class FazendaController : Controller
     {
         private BancoContext db = new BancoContext();
@@ -26,7 +27,8 @@ namespace OrganWeb.Areas.Sistema.Controllers
             {
                 Funcionario = db.Funcionarios
                     .Include(f => f.Cargo)
-                    .ToList()
+                    .ToList(),
+                Fazenda = db.Fazendas.Include(f => f.Localizacao).ToList()
             };
 
             //var fazendas = db.Fazendas.Include(f => f.Localizacao).ToList();
@@ -34,112 +36,19 @@ namespace OrganWeb.Areas.Sistema.Controllers
             return View(fazenda);
         }
 
-        // GET: Sistema/Fazenda/Details/5
-        public ActionResult Details(int? id)
+        public partial class _Default : System.Web.UI.Page
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Fazenda fazenda = db.Fazendas.Find(id);
-            if (fazenda == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fazenda);
-        }
+            static bool _isSqlTypesLoaded = false;
 
-        // GET: Sistema/Fazenda/Create
-        public ActionResult Create()
-        {
-            ViewBag.CEP = new SelectList(db.Localizacaos, "CEP", "Endereco");
-            return View();
-        }
-
-        // POST: Sistema/Fazenda/Create
-        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
-        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Area,Perimetro,Coordenadas,CEP,Numero")] Fazenda fazenda)
-        {
-            if (ModelState.IsValid)
+            public _Default()
             {
-                db.Fazendas.Add(fazenda);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                if (!_isSqlTypesLoaded)
+                {
+                    SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~"));
+                    _isSqlTypesLoaded = true;
+                }
 
-            ViewBag.CEP = new SelectList(db.Localizacaos, "CEP", "Endereco", fazenda.CEP);
-            return View(fazenda);
-        }
-
-        // GET: Sistema/Fazenda/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Fazenda fazenda = db.Fazendas.Find(id);
-            if (fazenda == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CEP = new SelectList(db.Localizacaos, "CEP", "Endereco", fazenda.CEP);
-            return View(fazenda);
-        }
-
-        // POST: Sistema/Fazenda/Edit/5
-        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
-        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Area,Perimetro,Coordenadas,CEP,Numero")] Fazenda fazenda)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(fazenda).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CEP = new SelectList(db.Localizacaos, "CEP", "Endereco", fazenda.CEP);
-            return View(fazenda);
-        }
-
-        // GET: Sistema/Fazenda/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Fazenda fazenda = db.Fazendas.Find(id);
-            if (fazenda == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fazenda);
-        }
-
-        // POST: Sistema/Fazenda/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Fazenda fazenda = db.Fazendas.Find(id);
-            db.Fazendas.Remove(fazenda);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
