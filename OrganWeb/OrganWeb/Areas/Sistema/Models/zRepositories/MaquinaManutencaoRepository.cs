d1af37cb8963a2296881ec404a24108de6820261
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-09-19 18:31:11 -0300

    Manutenção selecionando com detalhes

---
+++ OrganWeb/OrganWeb/Areas/Sistema/Models/zRepositories/MaquinaManutencaoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OrganWeb.Models;
+using OrganWeb.Areas.Sistema.Models.Ferramentas;
+
+namespace OrganWeb.Areas.Sistema.Models.zRepositories
+{
+    public class MaquinaManutencaoRepository : Repository<ManutencaoMaquina>
+    {
+        public ManutencaoMaquina GetByID(int? man, int? maq)
+        {
+            return DbSet.Find(man, maq);
+        }
+    }
+}
