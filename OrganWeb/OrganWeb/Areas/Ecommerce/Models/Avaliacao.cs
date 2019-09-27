882164abf94aaddcf19959c8cf9c98ba2d0430ae
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-09-26 21:39:48 -0300

    Algumas classes ecommerce, GET API FretNet

---
+++ OrganWeb/OrganWeb/Areas/Ecommerce/Models/Avaliacao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OrganWeb.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OrganWeb.Areas.Ecommerce.Models
+{
+    public class Avaliacao
+    {
+        [Key]
+        [Column(Order = 1)]
+        [ForeignKey("Anuncio")]
+        public int IdAnuncio { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [ForeignKey("Usuario")]
+        public int IdUsuario { get; set; }
+
+        public int Nota { get; set; }
+        public int Like { get; set; }
+
+        public virtual Anuncio Anuncio { get; set; }
+        public virtual ApplicationUser Usuario { get; set; }
+    }
+}
---
+++ OrganWeb/OrganWeb/Areas/Ecommerce/Models/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OrganWeb.Areas.Ecommerce.Models
+{
+    public class Pedido
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public bool Status { get; set; }
+    }
+}
