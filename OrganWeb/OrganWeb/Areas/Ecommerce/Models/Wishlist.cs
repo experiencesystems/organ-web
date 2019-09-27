6d6de8dff47accc52b267ef10c91650bf96751ba
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-09-26 21:39:48 -0300

    Algumas classes ecommerce, GET API FretNet

---
+++ OrganWeb/OrganWeb/Areas/Ecommerce/Models/Wishlist.cs
@@ -0,0 +1,26 @@
+﻿using OrganWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OrganWeb.Areas.Ecommerce.Models
+{
+    public class Wishlist
+    {
+        [Key]
+        [Column(Order = 1)]
+        [ForeignKey("Anuncio")]
+        public int IdAnuncio { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [ForeignKey("Usuario")]
+        public int IdUsuario { get; set; }
+
+        public virtual Anuncio Anuncio { get; set; }
+        public virtual ApplicationUser Usuario { get; set; }
+    }
+}
