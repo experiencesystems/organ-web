7935ac1f4e74db3e77350b68c7f49ad2ec3f66e7
RAQUEL ALVES DA SILVA <raquel.silva352@etec.sp.gov.br>
2019-10-01 20:03:45 -0300

    view equipe iniciada, outras views arrumadas.

---
+++ OrganWeb/OrganWeb/Areas/Sistema/Models/ViewModels/ViewEquipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OrganWeb.Areas.Sistema.Models.Funcionarios;
+namespace OrganWeb.Areas.Sistema.Models.ViewModels
+{
+    public class ViewEquipe
+    {
+        public IEnumerable<FuncEquipe> FunciEquipe { get; set; }
+        public IEnumerable<Equipe> Equipes { get; set; }
+        public IEnumerable<Funcionario> Funcionarios { get; set; }
+    }
+}
