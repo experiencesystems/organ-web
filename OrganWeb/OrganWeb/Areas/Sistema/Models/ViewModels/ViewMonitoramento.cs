7a43688235979573162d82399a95ce0459389a2b
Raquel Alves <raquelalves3753@gmail.com>
2019-10-01 20:03:45 -0300

    view equipe iniciada, outras views arrumadas.

---
+++ OrganWeb/OrganWeb/Areas/Sistema/Models/ViewModels/ViewMonitoramento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OrganWeb.Areas.Sistema.Models;
+
+namespace OrganWeb.Areas.Sistema.Models.ViewModels
+{
+    public class ViewMonitoramento
+    {
+    }
+}
