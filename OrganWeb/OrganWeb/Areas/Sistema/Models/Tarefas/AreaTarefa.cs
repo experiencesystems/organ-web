4e3e9376c2aa7aae32d07e9c7d72e4a8d9443e41
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-10-01 17:52:47 -0300

    Models de acordo com o banco

---
+++ OrganWeb/OrganWeb/Areas/Sistema/Models/Tarefas/AreaTarefa.cs
@@ -0,0 +1,25 @@
+﻿using OrganWeb.Areas.Sistema.Models.Administrativo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OrganWeb.Areas.Sistema.Models.Tarefas
+{
+    [Table("tbAreaTarefa")]
+    public class AreaTarefa
+    {
+        [Key, Column(Order = 1)]
+        [ForeignKey("Tarefa")]
+        public int IdTarefa { get; set; }
+
+        [Key, Column(Order = 2)]
+        [ForeignKey("Area")]
+        public int IdArea { get; set; }
+
+        public virtual Tarefa Tarefa { get; set; }
+        public virtual Area Area { get; set; }
+    }
+}
