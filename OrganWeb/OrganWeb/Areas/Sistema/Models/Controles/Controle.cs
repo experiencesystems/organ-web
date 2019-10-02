9e54400566d5b9be4d150a6a5bd4aa52b10363cc
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-10-02 11:33:34 -0300

    Selecionando func líder na equipe, selecionando linha da praga

--- OrganWeb/OrganWeb/Areas/Sistema/Models/Controles/Controle.cs
+++ OrganWeb/OrganWeb/Areas/Sistema/Models/Controles/Controle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrganWeb.Models.Banco;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Web;
 namespace OrganWeb.Areas.Sistema.Models.Controles
 {
     [Table("tbControle")]
-    public class Controle
+    public class Controle : Repository<Controle>
     {
         [Key]
         public int Id { get; set; }
