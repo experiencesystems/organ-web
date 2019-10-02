6a1fc48524103180204b1eb4b7121549e88b54b2
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-10-02 12:56:50 -0300

    Merge from master

--- OrganWeb/OrganWeb/Areas/Sistema/Models/ViewModels/ViewFinanceiro.cs
+++ OrganWeb/OrganWeb/Areas/Sistema/Models/ViewModels/ViewFinanceiro.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using OrganWeb.Areas.Sistema.Models.Financas;
+using OrganWeb.Areas.Sistema.Models.ViewsBanco;
 
 namespace OrganWeb.Areas.Sistema.Models.ViewModels
 {
     public class ViewFinanceiro
     {
         public IEnumerable<Compra> Compras { get; set; }
+        public IEnumerable<Conta> Conta { get; set; }
         public IEnumerable<Despesa> Despesas { get; set; }
+        public IEnumerable<DespesaAdm> DespesaAdms { get; set; }
+        public IEnumerable<DespesaFunc> DespesaFuncs { get; set; }
+        public IEnumerable<VwCompra> VwCompras { get; set; }
+        public IEnumerable<VwSaldo> VwSaldos { get; set; }
+        public IEnumerable<VwFluxoDeCaixa> VwFluxoDeCaixas { get; set; }
     }
 }
