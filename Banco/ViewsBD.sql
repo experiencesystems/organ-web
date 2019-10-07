18aac98bbab7e43226171d64cb8688d421d9ecce
Victor Hugo Lopes <victandre_lopes@live.com>
2019-10-07 18:22:24 -0300

    Mudei o nome da query do Banco pra (ESaiaDeMansinho). Fiz as views da Praga/Doença e de Controle. Estou arrumando a Compra e consequentemente o fluxo de caixa (Talvez eu transforme em proc).

--- Banco/ViewsBD.sql
+++ Banco/ViewsBD.sql
@@ -42,39 +42,47 @@ use dbOrgan;
             (E.Qtd * E.ValorUnit),
             'Produto'
 	FROM tbProduto P
-	INNER JOIN tbEstoque E ON P.IdEstoque = E.Id);
+	INNER JOIN tbEstoque E ON P.IdEstoque = E.Id)
+    order by `Categoria`;
 -- ===============================================================================================================================  
 
 -- =================================================================== FLUXO DE CAIXA ============================================  
 	drop view if exists vwCompra;
     create view vwCompra as(
-    select C.Id 'Compra', DATE_FORMAT(C.`Data`, '%d/%m/%Y') `Data`, I.Item, E.Id 'Estoque',
-    SUM(((IC.QtdProd * E.ValorUnit) - ((IC.QtdProd * E.ValorUnit) * IC.DescontoProd)) - 
-    (((IC.QtdProd * E.ValorUnit) - ((IC.QtdProd * E.ValorUnit) * IC.DescontoProd)) * C.Desconto)) `Valor Total`
+    select C.Id 'Compra', DATE_FORMAT(C.`Data`, '%d/%m/%Y') `Data`,
+		   group_concat(distinct concat(I.Item, ' - ', IC.QtdProd) separator ', ') `Itens - Quantidade Comprada`,    E.Id 'Estoque',
+		   SUM(
+           ((IC.QtdProd * E.ValorUnit) - ((IC.QtdProd * E.ValorUnit) * IC.DescontoProd)) - 
+		   (((IC.QtdProd * E.ValorUnit) - ((IC.QtdProd * E.ValorUnit) * IC.DescontoProd)) * C.Desconto)
+           ) `Valor Total`
 		from tbItensComprados IC INNER JOIN tbEstoque E on IC.IdEstoque = E.Id
 								 INNER JOIN tbCompra C on IC.IdCompra = C.Id
                                  INNER JOIN vwItems I on I.Id = E.Id
+	group by Compra
     );                             
 	
     drop view if exists vwSaida;
-	create View vwSaida as select Co.`Data`, (SUM(M.ValorPago) + SUM(D.ValorPago)  + SUM(Co.`Valor Total`)) `Saída` from tbManutencao M, tbDespesa D, vwCompra Co;
+	create View vwSaida as select DATE_FORMAT(Co.`Data`, '%d/%m/%Y') `Data`, (SUM(M.ValorPago) + SUM(D.ValorPago)  + SUM(Co.`Valor Total`)) `Saída` from tbManutencao M, tbDespesa D, vwCompra Co group by Co.`Data`;
 
 	drop view if exists vwVenda;
 	create view vwVenda as(
-    select V.Id 'IdVenda',V.`Data` `D`, (DATE_FORMAT(V.`Data`, '%d/%m/%Y')) `Data`, E.Id 'IdEstoque',
+    select V.Id `Venda`, (DATE_FORMAT(V.`Data`, '%d/%m/%Y')) `Data`, E.Id 'IdEstoque',
     SUM(((IV.QtdVendida * E.ValorUnit) - ((IV.QtdVendida * E.ValorUnit) * IV.DescontoProd))
     - (((IV.QtdVendida * E.ValorUnit)
     - ((IV.QtdVendida * E.ValorUnit) * IV.DescontoProd)) * V.Desconto)) ValorTotal
 		from tbItensVendidos IV INNER JOIN tbEstoque E on IV.IdEstoque = E.Id
 								INNER JOIN tbVenda V on IV.IdVenda = V.Id
 	);
-	
+    
     drop view if exists vwSaldo;
-    create view vwSaldo as select  (IFNULL(V.ValorTotal, 0) - IFNULL(S.Saída, 0))  `Saldo` from vwSaida S, vwVenda V;
+    create view vwSaldo as select (IFNULL(V.ValorTotal, 0) - IFNULL(S.Saída, 0))  `Saldo` from vwSaida S, vwVenda V;
 	
-    drop view if exists vwFluxoDeCaixa;
+    drop view if exists vwFluxoDeCaixa; 
     create view vwFluxoDeCaixa as
-    select IFNULL(S.`Saída`,0) `Saída`, IFNULL(V.ValorTotal,0) `Entrada`, Sal.Saldo, monthname(S.`Data`) `MÊS` from vwVenda V, vwSaida S, vwSaldo Sal where month(S.`Data`) = month(V.D) group by `MÊS`;
+    select IFNULL(S.`Saída`,0) `Saída`, IFNULL(V.ValorTotal, 0) `Entrada`, Sal.Saldo, monthname(S.`Data`) `MÊS`, year(S.`Data`) `ANO`
+    from vwVenda V, vwSaida S, vwSaldo Sal
+    where S.`Data` = V.`Data`
+    group by `ANO`; 
     
     
 -- MUDAR VALOR DOS NOMES DAS DATAS PRA PORTUGUES    SET lc_time_names = 'pt_BR';
@@ -207,13 +215,12 @@ use dbOrgan;
 			insert into tbProduto(IdEstoque, Nome, `Desc`) value(IdE, Nome, `Desc`);
 		end if;
     end$$
-
+/*
     call spInsertSemente(2, 1, 1.50, 'Semente de Milho', null, null, null, null)$$
     call spInsertProduto(1, 1, 5.0, 'Milho', null)$$
     call spInsertInsumo(1, 3, 2.0, 'Pá', null, 2)$$
-    call spInsertMaquina(1, 3, 2500, 'Tratorzinho', 1, 'Joana Motors', null, 2, 2300, 20, 240)$$
+    call spInsertMaquina(1, 3, 2500, 'Tratorzinho', 1, 'Joana Motors', null, 2, 2300, 20, 240)$$*/
     DELIMITER ;
-    select * from vwItems;
 -- =============================================================================================================================== 
 -- =================================================================== MANUTENÇÃO ================================================
 drop view if exists vwQtdMa;
@@ -234,14 +241,32 @@ select M.Nome `Máquina`, M.Tipo `Tipo de Máquina`, Ma.Nome `Manutenção`, (DA
 -- =============================================================================================================================== 
 
 -- =================================================================== PROC ESTOQUE ===============================================
-    select AC.`Praga/Doença`, count(C.Id) `Quantidade de Controles`/*, monthname(C.`Data`) `MÊS` */from vwAC AC inner join tbControle C on AC.Id = C.Id;
+	drop view if exists vwPragaOrDoenca ;
+     create view vwPragaOrDoenca as
+		select  pd.Nome `Nome`,
+        (case
+			when pd.`P/D` = true then 'Praga'
+            else'Doença'
+		end) as `Tipo`,
+		group_concat(a.Nome separator ', ') `Áreas`, c.`Status`
+		from tbAreaPD apd
+			inner join tbPragaOrDoenca pd on apd.IdPD = pd.Id
+            inner join tbArea a on apd.IdArea = a.Id
+            inner join tbControlePD cpd on cpd.IdPd = pd.Id
+            inner join tbControle c on c.Id = cpd.IdControle
+		group by `Nome`;
 	
-    create view vwAC as
-    select a.Nome `Área`, c.Id, pd.Nome `Praga/Doença`
-     from tbAreaPD apd
-     inner join tbArea a on apd.IdArea = a.Id
-     inner join tbPragaOrDoenca pd on apd.IdPD = pd.Id
-     inner join tbControlePD cpd on cpd.IdPD = pd.Id
-     inner join tbControle c on cpd.IdControle = c.Id;
-     
+    drop view if exists vwControle;
+    create view vwControle as
+    select DATE_FORMAT(c.`Data`, '%d/%m/%Y') `Data`, c.`Status`, ifnull(c.`Desc`, 'Sem Descrição') `Descrição`, c.Efic `Eficiência(%)`,
+		   c.NumLiberacoes `Número de Liberações`, group_concat(distinct p.Nome separator ', ' ) `Pragas/Doenças`,
+           group_concat(distinct concat(i.Item, ' - ', ic.QtdUsada) separator ', ') `Itens Usados - Quantidade`
+    from tbControle c
+		inner join tbControlePD cpd on c.Id = cpd.IdControle
+        inner join tbPragaOrDoenca p on cpd.IdPD = p.Id
+        inner join tbItensControle ic on c.Id = ic.IdControle
+        inner join vwItems i on ic.IdEstoque = i.Id
+	group by c.Id;
+    
 -- =============================================================================================================================== 
+
