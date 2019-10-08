d6083420954c5352ef16b70a874c7d3997a706f5
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-10-08 17:31:46 -0300

    CRUD Despesas funfando

--- OrganWeb/OrganWeb/Areas/Sistema/Models/ViewsBanco/Financeiro/VwSaida.cs
+++ OrganWeb/OrganWeb/Areas/Sistema/Models/ViewsBanco/Financeiro/VwSaida.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace OrganWeb.Areas.Sistema.Models.ViewsBanco.Financeiro
     [Table("vwSaida")]
     public class VwSaida
     {
+        [Key]
         public string Data { get; set; }
         public double Saida { get; set; }
     }
