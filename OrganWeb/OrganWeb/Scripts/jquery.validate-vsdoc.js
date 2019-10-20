1b91f97eee5e43b6bd914be6b35d0bd132b458ef
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-10-20 12:21:44 -0300

    Alteração nos jquery.val pra aceitar decimais em PT-BR, create da Colheita

--- OrganWeb/OrganWeb/Scripts/jquery.validate-vsdoc.js
+++ OrganWeb/OrganWeb/Scripts/jquery.validate-vsdoc.js
@@ -1147,9 +1147,9 @@ $.extend($.validator, {
 		},
 	
 		// http://docs.jquery.com/Plugins/Validation/Methods/number
-		number: function(value, element) {
-			return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/.test(value);
-		},
+        number: function (value, element) {
+            return this.optional(element) || /^(?:-?\d+|-?\d{1,3}(?:.\d{3})+)?(?:\,\d+)?$/.test(value);
+        },
 	
 		// http://docs.jquery.com/Plugins/Validation/Methods/digits
 		digits: function(value, element) {
