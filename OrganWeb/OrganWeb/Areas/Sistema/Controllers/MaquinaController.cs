b68b91ae2b0bb139c70865b42f09e4dac5b7d637
RAQUEL ALVES DA SILVA <raquel.silva352@etec.sp.gov.br>
2019-10-20 12:08:13 -0300

    maquina e fornecedor prontos create do praga or doenca pronto

--- OrganWeb/OrganWeb/Areas/Sistema/Controllers/MaquinaController.cs
+++ OrganWeb/OrganWeb/Areas/Sistema/Controllers/MaquinaController.cs
@@ -3,6 +3,7 @@ using OrganWeb.Areas.Sistema.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,6 +20,93 @@ namespace OrganWeb.Areas.Sistema.Controllers
                 Maquina = maquina.GetFew()
             };
             return View(select);
+        }
+
+        public ActionResult Create()
+        {
+           
+            // n sei oq isso aki retornaria sinceramente vo v
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Maquina maquina)
+        {
+            if (ModelState.IsValid)
+            {
+                var maq = new Maquina
+                {
+                    Nome = maquina.Nome,
+                    Tipo = maquina.Tipo,
+                    Desc = maquina.Desc,
+                    VidaUtil = maquina.VidaUtil,
+                    ValorInicial = maquina.ValorInicial,
+                    DeprMes = maquina.DeprMes,
+                    DeprAno = maquina.DeprAno,
+                    DataCadastro = maquina.DataCadastro,
+                };
+                maq.Add(maq);
+                maq.Save();
+
+                
+
+            }
+            return View(maquina);
+        }
+
+        public ActionResult Excluir(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            maquina = maquina.GetByID(id);
+            if (maquina == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(maquina);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirConfirmado(Maquina maquina)
+        {
+            
+            maquina = maquina.GetByID(maquina.IdEstoque);
+            maquina.Delete(maquina.IdEstoque);
+            maquina.Save();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            maquina = maquina.GetByID(id);
+            if (maquina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maquina);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Maquina maquina)
+        {
+           
+             if (ModelState.IsValid)
+            {
+                maquina.Update(maquina);
+                maquina.Save();
+                return RedirectToAction("Index");
+            }
+            return View(maquina);
         }
     }
 }
