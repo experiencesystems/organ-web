c43e4ac6a5c5116950bbf8750c67df9f20e25eb9
Natalya <natthy.aragao@gmail.com>
2019-11-15 21:04:11 -0300

    script para adicionar campos

---
+++ OrganWeb/OrganWeb/Scripts/btndinamico.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    //group add limit
+    var maxGroup = 10;
+
+    //add more fields group
+    $(".addMore").click(function () {
+        if ($('body').find('.fieldGroup').length < maxGroup) {
+            var fieldHTML = '<div class="form-group fieldGroup">' + $(".fieldGroupCopy").html() + '</div>';
+            $('body').find('.fieldGroup:last').after(fieldHTML);
+        } else {
+            alert('Maximum ' + maxGroup + ' groups are allowed.');
+        }
+    });
+
+    //remove fields group
+    $("body").on("click", ".remove", function () {
+        $(this).parents(".fieldGroup").remove();
+    });
+});
